4958c5ae0ad594dddfe793e07786d8a76eb4b5a5
Lucas B Ribeiro <br.lucasribeiro@gmail.com>
2023-06-23 19:56:38 -0300

    Template Method

--- src/CriadorDePersonagem.java
+++ src/CriadorDePersonagem.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class CriadorDePersonagem {
+abstract public class CriadorDePersonagem {
+    public void criarPersonagem() {
+        EscolherTipoDoPersonagem();
+        CriarAtributosDoPersonagem();
+        EquiparPersonagem();
+    }
+
+    protected abstract void EscolherTipoDoPersonagem();
+    protected abstract void CriarAtributosDoPersonagem();
+    protected abstract void EquiparPersonagem();
+}
+
+class CriadorDeLadino extends CriadorDePersonagem {
+    protected void EscolherTipoDoPersonagem() {
+        System.out.println("Criando um Ladino...");
+    }
+
+    protected void CriarAtributosDoPersonagem() {
+        System.out.println("Atributos do Ladino criados.");
+    }
+
+    protected void EquiparPersonagem() {
+        System.out.println("Equipando o Ladino com uma espada.");
+    }
+
+}
+
+class CriadorDePaladino extends CriadorDePersonagem {
+    protected void EscolherTipoDoPersonagem() {
+        System.out.println("Criando um Paladino...");
+    }
+
+    protected void CriarAtributosDoPersonagem() {
+        System.out.println("Atributos do Paladino criados.");
+    }
+
+    protected void EquiparPersonagem() {
+        System.out.println("Equipando o Paladino com um escudo.");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CriadorDePersonagem criador = new CriadorDeLadino();
+        criador.criarPersonagem();
+
+        System.out.println();
+
+        criador = new CriadorDePaladino();
+        criador.criarPersonagem();
+
     }
 }
